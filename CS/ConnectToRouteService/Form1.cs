910d72e62a2dc90a06091d57844e97e5270b712d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ConnectToRouteService/Form1.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.XtraMap;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ConnectToRouteService {
+    public partial class Form1 : Form {
+        const string bingKey = "YOUR BING KEY";
+        InformationLayer RouteLayer { get { return (InformationLayer)mapControl1.Layers["RouteLayer"]; } }
+        BingRouteDataProvider RouteProvider { get { return (BingRouteDataProvider)RouteLayer.DataProvider; } }
+
+        public Form1() {
+            InitializeComponent();
+            RouteLayer.DataProvider = new BingRouteDataProvider() { BingKey = bingKey };
+        }
+        
+        private void Form1_Load(object sender, System.EventArgs e) {
+            #region #Waypoints
+            // Create three waypoints and add them to a route waypoints list. 
+            List<RouteWaypoint> waypoints = new List<RouteWaypoint>();
+            waypoints.Add(new RouteWaypoint("New York", new GeoPoint(41.145556, -73.995)));
+            waypoints.Add(new RouteWaypoint("Oklahoma", new GeoPoint(36.131389, -95.937222)));
+            waypoints.Add(new RouteWaypoint("Las Vegas", new GeoPoint(36.175, -115.136389)));
+            #endregion #Waypoints
+
+            // Call the BingRouteDataProvider.CalculateRoute method.
+            RouteProvider.CalculateRoute(waypoints);
+
+            // Handle the BingRouteDataProvider.LayerItemsGenerating event.
+            RouteProvider.LayerItemsGenerating += routeLayerItemsGenerating;
+            RouteLayer.DataRequestCompleted += RouteLayer_DataRequestCompleted;
+        }
+
+        void RouteLayer_DataRequestCompleted(object sender, RequestCompletedEventArgs e) {
+            mapControl1.ZoomToFitLayerItems();
+        }
+
+        private void routeLayerItemsGenerating(object sender, LayerItemsGeneratingEventArgs e) {
+            if (e.Cancelled || (e.Error != null)) return;
+            
+            char pushpinMarker = 'A';
+            foreach (MapItem item in e.Items) {
+                MapPushpin pushpin = item as MapPushpin;
+                if (pushpin != null) {
+                    pushpin.Text = pushpinMarker++.ToString();
+                }
+                MapPolyline polyline = item as MapPolyline;
+                if (polyline != null) {
+                    polyline.Stroke = Color.FromArgb(0xFF, 0xFE, 0x72, 0xFF);
+                    polyline.StrokeWidth = 4;
+                }
+            }
+        }
+    }
+}
---
+++ VB/ConnectToRouteService/Form1.vb
@@ -0,0 +1,68 @@
+﻿Imports DevExpress.XtraMap
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Windows.Forms
+
+Namespace ConnectToRouteService
+    Partial Public Class Form1
+        Inherits Form
+
+        Private Const bingKey As String = "YOUR BING KEY"
+        Private ReadOnly Property RouteLayer() As InformationLayer
+            Get
+                Return CType(mapControl1.Layers("RouteLayer"), InformationLayer)
+            End Get
+        End Property
+        Private ReadOnly Property RouteProvider() As BingRouteDataProvider
+            Get
+                Return CType(RouteLayer.DataProvider, BingRouteDataProvider)
+            End Get
+        End Property
+
+        Public Sub New()
+            InitializeComponent()
+            RouteLayer.DataProvider = New BingRouteDataProvider() With {.BingKey = bingKey}
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+'            #Region "#Waypoints"
+            ' Create three waypoints and add them to a route waypoints list. 
+            Dim waypoints As New List(Of RouteWaypoint)()
+            waypoints.Add(New RouteWaypoint("New York", New GeoPoint(41.145556, -73.995)))
+            waypoints.Add(New RouteWaypoint("Oklahoma", New GeoPoint(36.131389, -95.937222)))
+            waypoints.Add(New RouteWaypoint("Las Vegas", New GeoPoint(36.175, -115.136389)))
+'            #End Region ' #Waypoints
+
+            ' Call the BingRouteDataProvider.CalculateRoute method.
+            RouteProvider.CalculateRoute(waypoints)
+
+            ' Handle the BingRouteDataProvider.LayerItemsGenerating event.
+            AddHandler RouteProvider.LayerItemsGenerating, AddressOf routeLayerItemsGenerating
+            AddHandler RouteLayer.DataRequestCompleted, AddressOf RouteLayer_DataRequestCompleted
+        End Sub
+
+        Private Sub RouteLayer_DataRequestCompleted(ByVal sender As Object, ByVal e As RequestCompletedEventArgs)
+            mapControl1.ZoomToFitLayerItems()
+        End Sub
+
+        Private Sub routeLayerItemsGenerating(ByVal sender As Object, ByVal e As LayerItemsGeneratingEventArgs)
+            If e.Cancelled OrElse (e.Error IsNot Nothing) Then
+                Return
+            End If
+
+            Dim pushpinMarker As Char = "A"c
+            For Each item As MapItem In e.Items
+                Dim pushpin As MapPushpin = TryCast(item, MapPushpin)
+                If pushpin IsNot Nothing Then
+                    pushpin.Text = pushpinMarker.ToString()
+                    pushpinMarker = ChrW(AscW(pushpinMarker) + 1)
+                End If
+                Dim polyline As MapPolyline = TryCast(item, MapPolyline)
+                If polyline IsNot Nothing Then
+                    polyline.Stroke = Color.FromArgb(&HFF, &HFE, &H72, &HFF)
+                    polyline.StrokeWidth = 4
+                End If
+            Next item
+        End Sub
+    End Class
+End Namespace
