b01c5c8a5974512f56d7a4ac519cbc73858a60ad
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ConnectToRouteService/Form1.cs
@@ -0,0 +1,122 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraMap;
+
+namespace ConnectToRouteService {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        const string yourBingKey = "INSERT_YOUR_BING_KEY_HERE";
+        MapPolyline polyline = new MapPolyline();
+        List<RouteWaypoint> waypoints = new List<RouteWaypoint>();
+        BingRouteDataProvider routeProvider;
+        int waypointIndex = 0;
+
+        private void Form1_Load(object sender, System.EventArgs e) {
+            // Call the PrepareMap method.  
+            PrepareMap();
+
+            // Create three waypoints and add them to a route waypoints list. 
+            GeoPoint NY = new GeoPoint(41.145556, -73.995);
+            RouteWaypoint waypoint1 = new RouteWaypoint("NY", NY);
+            waypoints.Add(waypoint1);
+            GeoPoint Oklahoma = new GeoPoint(36.131389, -95.937222);
+            RouteWaypoint waypoint2 = new RouteWaypoint("Oklahoma", Oklahoma);
+            waypoints.Add(waypoint2);
+            GeoPoint LasVegas = new GeoPoint(36.175, -115.136389);
+            RouteWaypoint waypoint3 = new RouteWaypoint("Las Vegas", LasVegas);
+            waypoints.Add(waypoint3);
+
+            // Call the BingRouteDataProvider.CalculateRoute method.
+            routeProvider.CalculateRoute(waypoints);
+
+            // Handle the BingRouteDataProvider.RouteCalculated event.
+            routeProvider.RouteCalculated += new BingRouteCalculatedEventHandler(routeProvider_RouteCalculated);
+
+            // Handle the BingRouteDataProvider.LayerItemsGenerating event.
+            routeProvider.LayerItemsGenerating += new LayerItemsGeneratingEventHandler(routeLayerItemsGenerating);
+        }
+
+
+        private void routeProvider_RouteCalculated(object sender, BingRouteCalculatedEventArgs e) {
+
+            RouteCalculationResult result = e.CalculationResult;
+
+            if (result.ResultCode == RequestResultCode.Success) {
+                polyline.Points.AddRange(result.RouteResults[0].RoutePath);
+
+                // Customize the appearance of the calculated route path. 
+                polyline.Stroke = Color.FromArgb(0xFF, 0xFE, 0x72, 0xFF);
+                polyline.StrokeWidth = 4;
+            }
+        }
+
+        private void routeLayerItemsGenerating(object sender, LayerItemsGeneratingEventArgs e) {
+
+            if (e.Error == null && !e.Cancelled)
+                ProcessRouteItems(e.Items);
+        }
+
+        public void ProcessRouteItems(MapItem[] items) {
+
+            foreach (MapItem item in items) {
+                MapPushpin pushpin = item as MapPushpin;
+
+                if (pushpin != null) {
+                    pushpin.Text = NextWaypointLetter();
+                }
+            }
+        }
+
+        protected string NextWaypointLetter() {
+            waypointIndex++;
+            return waypointIndex.ToString(); ;
+        }
+
+        private void PrepareMap() {
+            // Create a map control.
+            MapControl map = new MapControl();
+
+            // Specify the map position on the form.           
+            map.Dock = DockStyle.Fill;
+
+            // Add the map control to the window.
+            this.Controls.Add(map);
+
+            // Create an image tiles layer and add it to the map.
+            ImageTilesLayer tilesLayer = new ImageTilesLayer();
+            map.Layers.Add(tilesLayer);
+
+            // Create an information layer and add it to the map.
+            InformationLayer infoLayer = new InformationLayer();
+            map.Layers.Add(infoLayer);
+
+            // Create a vector items layer and add it to the map.
+            VectorItemsLayer itemsLayer = new VectorItemsLayer();
+            MapItemStorage storage = new MapItemStorage();
+            itemsLayer.Data = storage;
+            map.Layers.Add(itemsLayer);
+
+            storage.Items.Add(polyline);
+
+            // Create a Bing data provider and specify the Bing key.
+            BingMapDataProvider bingProvider = new BingMapDataProvider();
+            tilesLayer.DataProvider = bingProvider;
+            bingProvider.BingKey = yourBingKey;
+
+            // Create a Bing route data provider and specify the Bing key.
+            routeProvider = new BingRouteDataProvider();
+            infoLayer.DataProvider = routeProvider;
+            routeProvider.BingKey = yourBingKey;
+
+            // Specify the map center point and zoom level. 
+            map.CenterPoint = new GeoPoint(37, -92);
+            map.ZoomLevel = 4;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,20 @@
+# How to calculate a route between waypoints and customize the appearance of a route path using a Microsoft Bing Route web service
+
+
+<p>This example demonstrates how to calculate a route between several waypoints and change the appearance of a route path by means of the Microsoft Bing Route web service.</p>
+<p> </p>
+<p> </p>
+
+
+<h3>Description</h3>
+
+To calculate a route between waypoints, call the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteDataProvider_CalculateRoutetopic"><u>BingRouteDataProvider.CalculateRoute</u></a> method and pass a list of waypoints as its argument.
+<p>&nbsp;</p>
+<p>To customize the route path appearance, do the following:</p>
+<p>- Handle the&nbsp; <a href="https://isc.devexpress.com/Thread/WorkplaceDetails/BingRouteDataProvider.RouteCalculated"><u>BingRouteDataProvider.RouteCalculated</u></a> event;<br /> - Access the route path using the <a href="http://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapRouteCalculationResulttopic"><u>RouteCalculationResult</u></a> object inside t<a href="http://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapBingRouteCalculatedEventArgstopic">he&nbsp; <strong>BingRouteDataProvider.RouteCalculated</strong> event handler's <u>BingRouteCalculatedEventArgs</u></a>;<br /> - Create a <a href="http://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapMapPolylinetopic"><u>MapPolyline</u></a><u> </u>with calculated route points. To access the route points collection, use the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteResult_RoutePathtopic"><u>BingRouteResult.RoutePath</u></a> property;<br /> - Customize the stroke and stroke width of the map polyline using the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMapItem_Stroketopic"><u>MapItem.Stroke</u></a> and <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMapItem_StrokeWidthtopic"><u>MapItem.StrokeWidth</u></a> properties.</p>
+<p>The map pushpins are generated automatically at each route waypoint position&nbsp; because the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapInformationDataProviderBase_GenerateLayerItemstopic"><u>InformationDataProviderBase.GenerateLayerItems</u></a><u> </u>property is set to <strong>true</strong> by default.&nbsp; Note that&nbsp; the pushpins' text contains an empty string. <br /> To number route waypoints (from start to finish) in the pushpin, handle the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapInformationDataProviderBase_LayerItemsGeneratingtopic"><u>InformationDataProviderBase.LayerItemsGenerating</u></a><u> </u>event and modify the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMapPointer_Texttopic"><u>MapPointer.Text</u></a> property.</p>
+<p>Note that if you run this sample as is, you will get a warning message saying that the specified Bing Maps key is invalid. To learn more about Bing Map keys, please refer to the <a href="http://help.devexpress.com/#WindowsForms/CustomDocument15102"><u>How to: Get a Bing Maps Key</u></a><u> </u>tutorial.</p>
+
+<br/>
+
+
