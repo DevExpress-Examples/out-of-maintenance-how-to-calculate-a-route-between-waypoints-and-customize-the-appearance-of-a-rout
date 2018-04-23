5e9593502eb7f799daf99ef399afb8355cd3c88e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ConnectToRouteService/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace ConnectToRouteService {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.splashScreenManager = new DevExpress.XtraSplashScreen.SplashScreenManager(this, typeof(global::ConnectToRouteService.WaitForm1), true, true);
+            this.imageLayer1 = new DevExpress.XtraMap.ImageLayer();
+            this.imageProvider = new DevExpress.XtraMap.BingMapDataProvider();
+            this.informationLayer = new DevExpress.XtraMap.InformationLayer();
+            this.mapControl = new DevExpress.XtraMap.MapControl();
+            this.defaultLookAndFeel = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splashScreenManager
+            // 
+            this.splashScreenManager.ClosingDelay = 500;
+            // 
+            // imageLayer1
+            // 
+            this.imageLayer1.DataProvider = this.imageProvider;
+            // 
+            // informationLayer
+            // 
+            this.informationLayer.EnableHighlighting = false;
+            this.informationLayer.EnableSelection = false;
+            this.informationLayer.Name = "RouteLayer";
+            // 
+            // mapControl
+            // 
+            this.mapControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapControl.Layers.Add(this.imageLayer1);
+            this.mapControl.Layers.Add(this.informationLayer);
+            this.mapControl.Location = new System.Drawing.Point(0, 0);
+            this.mapControl.Name = "mapControl";
+            this.mapControl.Size = new System.Drawing.Size(640, 360);
+            this.mapControl.TabIndex = 0;
+            // 
+            // defaultLookAndFeel
+            // 
+            this.defaultLookAndFeel.LookAndFeel.SkinName = "Office 2016 Colorful";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.Controls.Add(this.mapControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraMap.MapControl mapControl;
+        private DevExpress.XtraMap.ImageLayer imageLayer1;
+        private DevExpress.XtraMap.BingMapDataProvider imageProvider;
+        private DevExpress.XtraMap.InformationLayer informationLayer;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel;
+        private DevExpress.XtraSplashScreen.SplashScreenManager splashScreenManager;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to calculate a route between waypoints and customize the appearance of a route path using a Microsoft Bing Route web service
+
+
+<p>This example demonstrates how to calculate a route between several waypoints and change the appearance of a route path by means of the Microsoft Bing Route web service.</p>
+<p> </p>
+<p> </p>
+
+<br/>
+
+
